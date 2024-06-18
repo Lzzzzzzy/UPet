1cb791e7b303fe000453f450d75a01028f8d2616
Lzzzzzzy <1093813574@qq.com>
2024-06-18 23:00:51 +0800

    add vant

---
+++ assets/src/pages/record/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <van-calendar
+      title="日历"
+      :poppable="false"
+      :show-confirm="false"
+      class="calendar"
+    />
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+    }
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.calendar {
+  --calendar-height: 618px;
+}
+</style>
