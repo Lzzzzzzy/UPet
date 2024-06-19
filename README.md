2544f44e9cd74b93e80a9e96f5f0de59789d9823
EthanLiu <ethan.liu@yamibuy.com>
2024-06-19 18:06:30 +0800

    feat(front): 添加首页页面

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## 前端技术栈
 nodejs v21+
 npm 10+
-mpvue + mpvue-router-patch + mpvue-entry + vuex + webpack + ES6/7 + flyio + mpvue-wxparse
+taro + taro-ui-vue3
 
 ## 后端技术栈
 python3 + mysql + flask
---
+++ assets/src/app.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import { Input, Button, Popup, Calendar, Price, CalendarCard, Steps } from '@nutui/nutui-taro'
+import { IconFont } from '@nutui/icons-vue-taro'
+import './app.scss'
+import "@nutui/nutui-taro/dist/style.css";
+
+const App = createApp({
+  onShow (options) {},
+  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+})
+App.use(Button).use(Input).use(Popup).use(Calendar).use(Price).use(IconFont).use(CalendarCard).use(Steps);
+
+export default App
