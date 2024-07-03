d993796b32a4f3ce3f327180ebd61bd41a1968ce
LiuZhiyi <liu_zhiyi@126.com>
2024-07-03 23:38:09 +0800

    style(calendar): 修改日历样式

--- assets2/src/components/calendar/full-calendar/index.vue
+++ assets2/src/components/calendar/full-calendar/index.vue
@@ -1,31 +1,15 @@
 <template>
   <div>
     <div class="index-page">
-      <nut-calendar-card v-model="currentDate" class="card-info" ref="calendarRef">
+      <nut-calendar-card v-model="currentDate" class="card-info" ref="calendarRef" firstDayOfWeek="1">
+        <template #default="{ day }">
+          {{ day.date <= 9 ? `0${day.date}` : day.date }}
+        </template>
         <template #bottom="{ day }">
           <div :ref="(el)=>setDayRefs(el, day)">{{ isToday(day) ? "今天": "" }}</div>
         </template>
       </nut-calendar-card>
     </div>
-    <div class="subtitle">
-      <span>毛孩子的今天</span>
-      <nut-tabs v-model="currentTab" align="left" :animated-time="0">
-        <nut-tab-pane v-for="tab in tabList" :key="tab.paneKey" :title="tab.title" :pane-key="tab.paneKey"></nut-tab-pane>
-      </nut-tabs>
-    </div>
-    <div class="step-container">
-      <nut-steps direction="vertical">
-        <nut-step v-for="info in data" :key="info.id" :title="info.title" :content="info.content">
-          <template #icon>
-            123
-          </template>
-        </nut-step>
-      </nut-steps>
-    </div>
-    <div class="record-button-container">
-      <nut-button plain type="info">记一笔</nut-button>
-    </div>
-    <!-- <tabbar activate="calendar"></tabbar> -->
   </div>
 </template>
 
@@ -42,7 +26,7 @@ onBeforeMount(()=>{
 
 const isToday = (day) => {
   const today = new Date();
-  return day.date === today.getDate() && day.month === today.getMonth()+1;
+  return day.date === today.getDate() && day.month === today.getMonth()+1 && day.year === today.getFullYear();
 }
 
 const calendarRef = ref();
@@ -60,22 +44,6 @@ onMounted(()=>{
     let month = currentMonth;
     let year = currentYear;
     if (day.classList && day.classList.contains('nut-calendarcard-day')) {
-      if (day.classList.contains("prev")) {  // 本月如果是1月，那上个月应该是去年的12月
-        if (currentMonth === 1) {
-          month = 12;
-          year = currentYear - 1;
-        } else {
-          month = currentMonth - 1;
-        }
-      } else if (day.classList.contains("next")) {  // 本月如果是12月，那下个月应该是明年的1月
-        if (currentMonth === 12) {
-          month = 1;
-          year = currentYear + 1;
-        } else {
-          month = currentMonth + 1;
-        }
-      }
-
       const date = day.childNodes[2].childNodes[1].data;
       const key = `${year}-${month}-${date}`
       dayRefs.value[key] = day;
@@ -127,17 +95,10 @@ const data = ref([{"title": "喂食", "content": "喂了一半", "id": 1, "type"
 </script>
 
 <style lang="scss">
-.index-page {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  .card-info {
-    background: inherit;
-  }
+.card-info {
+  background: inherit;
 }
+
 
 .step-container {
   padding: 3rpx 10rpx;
@@ -149,6 +110,13 @@ const data = ref([{"title": "喂食", "content": "喂了一半", "id": 1, "type"
 }
 
 .nut-calendarcard-day {
+  &.next {
+    display: none;
+  }
+
+  &.prev {
+    visibility: hidden;
+  }
   
   &.active{
     background-color: initial;
@@ -163,46 +131,5 @@ const data = ref([{"title": "喂食", "content": "喂了一半", "id": 1, "type"
       color: var(--nut-primary-color);
     }
   }
-}
-
-.nut-step-main {
-  margin-bottom: 40rpx;
-}
-
-.subtitle {
-  padding: 0 10rpx;
-  margin: 10rpx 0 20rpx;
-  font-size: 38rpx;
-  font-weight: 600;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.custom-title {
-  color: black;
-  cursor: pointer;
-}
-
-.custom-title.active {
-  color: red;
-}
-
-.nut-tab-pane {
-  padding: 0 !important;
-}
-
-.record-button-container {
-  position: fixed;
-  right: 30rpx;
-  bottom: 60rpx;
-}
-
-.highlight {
-  background-color: yellow;
-}
-
-.special {
-  background-color: lightblue;
 }
 </style>
