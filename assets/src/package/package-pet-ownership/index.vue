04822e950d8957399075f902a7db371cff182ed0
LiuZhiyi <liu_zhiyi@126.com>
2024-07-22 16:55:42 +0800

    style(family management): 修改共养家庭页面样式

--- assets/src/package/package-pet-ownership/index.vue
+++ assets/src/package/package-pet-ownership/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue';
+import { onBeforeMount, ref, computed } from 'vue';
 import type { UserInfo } from "@/typings/family";
 
 const getAllFamilyMember = () => {
@@ -19,7 +19,7 @@ const getAllFamilyMember = () => {
     {
       "id": 3,
       "name": "对象",
-      "isAdmin": true,
+      "isAdmin": false,
       "avatar": ""
     },
 ]
@@ -27,31 +27,38 @@ const getAllFamilyMember = () => {
 
 const familyMembers = ref<Array<UserInfo>>();
 
-const mainSpanWidth = ref(8);
+const mainSpanWidth = ref(12);
 
 onBeforeMount(() => {
   familyMembers.value = getAllFamilyMember();
   if (!me.isAdmin) {
-    mainSpanWidth.value = 16;
+    mainSpanWidth.value = 20;
   }
 })
 
 const me = {
   "id": 4,
-  "name": "我自己",
+  "name": "刘",
   "isAdmin": true,
   "avatar": ""
 }
 
 const showDeleteConfirmPopup = ref(false);
-const needDeleteMemberId = ref();
-const deleteMember = (memberId: number) => {
+const needDeleteMember = ref<UserInfo>();
+const deleteConfirmText = computed(()=>{
+  if (needDeleteMember.value.id === me.id) {
+    return "确定要退出家庭吗？"
+  }
+  return `确定要移除家庭成员 ${needDeleteMember.value.name} 吗？`
+});
+
+const deleteMember = (member: UserInfo) => {
   showDeleteConfirmPopup.value = true;
-  needDeleteMemberId.value = memberId;
+  needDeleteMember.value = member;
 }
 const closeMemberPopup = () => {
   showDeleteConfirmPopup.value = false;
-  needDeleteMemberId.value = null;
+  needDeleteMember.value = null;
 }
 const confirmDelete = () => {
   closeMemberPopup();
@@ -61,14 +68,18 @@ const addMember = () => {};
 
 
 const showGrantConfirmPopup = ref(false);
-const needGrantMemberId = ref();
-const grantMember = (memberId: number) => {
+const needGrantMember = ref<UserInfo>();
+const grantConfirmText = computed(()=>{
+  return `确定要转让管理权限给 ${needGrantMember.value.name} 吗？`
+});
+
+const grantMember = (member: UserInfo) => {
   showGrantConfirmPopup.value = true;
-  needGrantMemberId.value = memberId;
+  needGrantMember.value = member;
 }
 const closeGrantPopup = () => {
   showGrantConfirmPopup.value = false;
-  needGrantMemberId.value = null;
+  needGrantMember.value = null;
 }
 const confirmGrant = () => {
   closeGrantPopup();
@@ -77,37 +88,59 @@ const confirmGrant = () => {
 <template>
 <basic-layout>
   <custom-navbar title="爱宠共养" left-show />
+  <div class="bg-white py-30px svg-bg pr-10px">
+    <nut-row :gutter="10" type="flex" justify="space-around" align="center">
+      <nut-col :span="4">
+          <div class="flex items-center justify-end">
+              <pet-avatar :avatar-img-url="me.avatar" />
+          </div>
+      </nut-col>
+      <nut-col :span="20">
+          <div class="flex flex-col items-start justify-center">
+              <div class="text-18px">
+                  {{ me.name }}
+              </div>
+              <div class="mt-10px">
+                <nut-avatar size="small" class="!flex-center !bg-#98c7ce" @click="deleteMember(me.id)" v-if="!me.isAdmin">
+                  <div class="text-20px i-local-exit"></div>
+                </nut-avatar>
+                <div class="text-12px text-coolgray" v-if="me.isAdmin">管理员</div>
+              </div>
+          </div>
+      </nut-col>
+  </nut-row>
+  </div>
   <div class="m-20px b-rd-16px py-30px bg-white" v-for="member in familyMembers" :key="member.id">
       <nut-row :gutter="10" type="flex" justify="space-around" align="center">
-          <nut-col :span="8">
-              <div class="flex items-center justify-center">
-                  <pet-avatar :avatar-img-url="member.avatar" size="large" />
+          <nut-col :span="4">
+              <div class="flex items-center justify-end">
+                  <pet-avatar :avatar-img-url="member.avatar" />
               </div>
           </nut-col>
           <nut-col :span="mainSpanWidth">
               <div class="flex flex-col items-start justify-center">
-                  <div class="text-20px">
+                  <div class="text-18px">
                       {{ member.name }}
                   </div>
-                  <div class="flex-center mt-10px">
-                    <div class="i-local-key text-15px ml-5px" v-if="member.isAdmin"></div>
+                  <div class="flex-center mt-10px" v-if="member.isAdmin">
+                    <div class="text-12px text-coolgray">管理员</div>
                   </div>
               </div>
           </nut-col>
           <nut-col :span="4" v-if="me.isAdmin">
-              <nut-avatar size="small" class="!flex-center !bg-#98c7ce" @click="grantMember(member.id)">
+              <nut-avatar size="small" class="!flex-center !bg-#98c7ce" @click="grantMember(member)">
                   <div class="text-20px i-local-grant"></div>
               </nut-avatar>
           </nut-col>
           <nut-col :span="4" v-if="me.isAdmin">
-            <nut-avatar size="small" class="!flex-center !bg-#98c7ce" @click="deleteMember(member.id)">
-              <div class="text-20px i-local-exit"></div>
+            <nut-avatar size="small" class="!flex-center !bg-#98c7ce" @click="deleteMember(member)">
+              <div class="text-20px i-local-delete"></div>
             </nut-avatar>
           </nut-col>
       </nut-row>
   </div>
 
-  <div class="flex-center">
+  <div class="flex-center" v-if="me.isAdmin">
       <nut-button @click="addMember" color="#f7daa1" class="!text-black">
           <div class="flex-center">
               <div class="text-15px i-local-add pr-5px"></div>
@@ -119,7 +152,7 @@ const confirmGrant = () => {
   <nut-popup v-model:visible="showDeleteConfirmPopup" round :style="{'width': '70%'}">
     <div class="px-20px py-40px">
       <div class="flex-col-center">
-        <div>确定要移除家庭成员吗?</div>
+        <div>{{ deleteConfirmText }}</div>
       </div>
       <div class="mt-20px flex justify-evenly">
         <nut-button plain @click="confirmDelete">确定</nut-button>
@@ -131,8 +164,8 @@ const confirmGrant = () => {
   <nut-popup v-model:visible="showGrantConfirmPopup" round :style="{'width': '70%'}">
     <div class="px-20px py-40px">
       <div class="flex-col-center">
-        <div>确定要转让管理权限吗？</div>
-        <div class="mt-20px text-12px text-coolgray">您将无法管理共养家庭</div>
+        <div>{{ grantConfirmText }}</div>
+        <div class="mt-20px text-12px text-coolgray">转让后您将无法管理家庭和宠物档案</div>
       </div>
       <div class="mt-20px flex justify-evenly">
         <nut-button plain @click="confirmGrant">确定</nut-button>
@@ -144,4 +177,11 @@ const confirmGrant = () => {
 </template>
 
 
-<style lang="scss"></style>
+<style lang="scss">
+.svg-bg {
+  background-image: url('../../assets/svg/house.svg');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: right;
+}
+</style>
