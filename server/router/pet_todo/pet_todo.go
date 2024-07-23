c389a9825f0082403e89d4f96ed83074c81f2dde
LiuZhiyi <liu_zhiyi@126.com>
2024-07-23 23:03:43 +0800

    fix(docs): 修改错误注释

--- server/router/pet_todo/pet_todo.go
+++ server/router/pet_todo/pet_todo.go
@@ -11,10 +11,10 @@ type PetTodoRouter struct{}
 func (e *PetTodoRouter) InitPetTodoRouter(Router *gin.RouterGroup) {
 	petRouter := Router.Group("petTodo").Use(middleware.OperationRecord())
 	{
-		petRouter.POST("pet-todo", petTodoApi.CreatePetInfo)              // 创建宠物
-		petRouter.PUT("pet-todo/:petTodoID", petTodoApi.UpdatePetInfo)    // 更新宠物
-		petRouter.DELETE("pet-todo/:petTodoID", petTodoApi.DeletePetInfo) // 删除宠物
-		petRouter.GET("pet-todo/:petTodoID", petTodoApi.GetPetInfo)       // 获取单一宠物信息
-		petRouter.GET("pet-todos", petTodoApi.GetPetInfoList)             // 获取宠物列表
+		petRouter.POST("pet-todo", petTodoApi.CreatePetInfo)              // 创建宠物待办
+		petRouter.PUT("pet-todo/:petTodoID", petTodoApi.UpdatePetInfo)    // 更新宠物待办
+		petRouter.DELETE("pet-todo/:petTodoID", petTodoApi.DeletePetInfo) // 删除宠物待办
+		petRouter.GET("pet-todo/:petTodoID", petTodoApi.GetPetInfo)       // 获取单一宠物待办信息
+		petRouter.GET("pet-todos", petTodoApi.GetPetInfoList)             // 获取宠物待办列表
 	}
 }
