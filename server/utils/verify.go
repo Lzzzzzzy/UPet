d1ecf85f26a58e3e09dae3424fbc98335cc57210
lzzzzzzy <liu_zhiyi@126.com>
2024-11-29 12:49:09 +0800

    fix(verify): remove useless validator

--- server/utils/verify.go
+++ server/utils/verify.go
@@ -1,21 +1,21 @@
 package utils
 
 var (
-	IdVerify               = Rules{"ID": []string{NotEmpty()}}
-	ApiVerify              = Rules{"Path": {NotEmpty()}, "Description": {NotEmpty()}, "ApiGroup": {NotEmpty()}, "Method": {NotEmpty()}}
-	MenuVerify             = Rules{"Path": {NotEmpty()}, "Name": {NotEmpty()}, "Component": {NotEmpty()}, "Sort": {Gt("-1")}}
-	MenuMetaVerify         = Rules{"Title": {NotEmpty()}}
-	LoginVerify            = Rules{"CaptchaId": {NotEmpty()}, "Username": {NotEmpty()}, "Password": {NotEmpty()}}
-	RegisterVerify         = Rules{"Username": {NotEmpty()}, "NickName": {NotEmpty()}, "Password": {NotEmpty()}, "AuthorityId": {NotEmpty()}}
-	PageInfoVerify         = Rules{"Page": {NotEmpty()}, "PageSize": {NotEmpty()}}
-	CustomerVerify         = Rules{"CustomerName": {NotEmpty()}, "CustomerPhoneData": {NotEmpty()}}
-	AutoCodeVerify         = Rules{"Abbreviation": {NotEmpty()}, "StructName": {NotEmpty()}, "PackageName": {NotEmpty()}, "Fields": {NotEmpty()}}
-	AutoPackageVerify      = Rules{"PackageName": {NotEmpty()}}
-	AuthorityVerify        = Rules{"AuthorityId": {NotEmpty()}, "AuthorityName": {NotEmpty()}}
-	AuthorityIdVerify      = Rules{"AuthorityId": {NotEmpty()}}
-	OldAuthorityVerify     = Rules{"OldAuthorityId": {NotEmpty()}}
-	ChangePasswordVerify   = Rules{"Password": {NotEmpty()}, "NewPassword": {NotEmpty()}}
-	SetUserAuthorityVerify = Rules{"AuthorityId": {NotEmpty()}}
+	IdVerify = Rules{"ID": []string{NotEmpty()}}
+	// ApiVerify              = Rules{"Path": {NotEmpty()}, "Description": {NotEmpty()}, "ApiGroup": {NotEmpty()}, "Method": {NotEmpty()}}
+	// MenuVerify             = Rules{"Path": {NotEmpty()}, "Name": {NotEmpty()}, "Component": {NotEmpty()}, "Sort": {Gt("-1")}}
+	// MenuMetaVerify         = Rules{"Title": {NotEmpty()}}
+	// LoginVerify            = Rules{"CaptchaId": {NotEmpty()}, "Username": {NotEmpty()}, "Password": {NotEmpty()}}
+	// RegisterVerify         = Rules{"Username": {NotEmpty()}, "NickName": {NotEmpty()}, "Password": {NotEmpty()}, "AuthorityId": {NotEmpty()}}
+	PageInfoVerify = Rules{"Page": {NotEmpty()}, "PageSize": {NotEmpty()}}
+	// CustomerVerify         = Rules{"CustomerName": {NotEmpty()}, "CustomerPhoneData": {NotEmpty()}}
+	// AutoCodeVerify         = Rules{"Abbreviation": {NotEmpty()}, "StructName": {NotEmpty()}, "PackageName": {NotEmpty()}, "Fields": {NotEmpty()}}
+	// AutoPackageVerify      = Rules{"PackageName": {NotEmpty()}}
+	// AuthorityVerify        = Rules{"AuthorityId": {NotEmpty()}, "AuthorityName": {NotEmpty()}}
+	// AuthorityIdVerify      = Rules{"AuthorityId": {NotEmpty()}}
+	// OldAuthorityVerify     = Rules{"OldAuthorityId": {NotEmpty()}}
+	// ChangePasswordVerify   = Rules{"Password": {NotEmpty()}, "NewPassword": {NotEmpty()}}
+	// SetUserAuthorityVerify = Rules{"AuthorityId": {NotEmpty()}}
 
 	PetInfoVerify     = Rules{"Name": {NotEmpty()}, "Birthday": {NotEmpty()}, "Gender": {Ge("0"), Lt("2")}, "Category": {Ge("0")}, "SterilizedState": {Ge("0"), Lt("2")}}
 	PetTodoInfoVerify = Rules{"Title": {NotEmpty()}, "Type": {Ge("0")}, "Color": {Ge("0")}, "TodoTime": {NotEmpty()}, "PetId": {NotEmpty()}}
